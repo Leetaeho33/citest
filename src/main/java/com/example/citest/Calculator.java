e9c0dc89c1a219a619835f2ebe88e068a9f4e766
taeholee <lth1993@naver.com>
2024-01-06 23:47:08 +0900

    빌드가 안될걸?2

--- src/main/java/com/example/citest/Calculator.java
+++ src/main/java/com/example/citest/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         return n+m;
     }
     public int minus(int n, int m){return n-m}
-    public int multiple(int n, int m) {return n*m;}
+    public int multiple(int n, int m) {return n*m}
 
     public int multipleTwo(int n, int m){
         return n*m;
